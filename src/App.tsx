fab173af395c2a465e2c729c8f5cd5475b0341b9
Abdullah Asmy <abdallaasmi14@gmail.com>
2026-05-07 14:51:08 -0700

    fix: correct mismatched question images

--- src/App.tsx
+++ src/App.tsx
@@ -41,50 +41,52 @@ const onboardingQuestionIds = new Set<string>([questionIds.role, questionIds.tim
 const promptUsesSectionTitle = new Set<string>([questionIds.bodyMakeshiftTool, questionIds.handheldToolContact, questionIds.upperBodyPosture, questionIds.headPosition]);
 
 const standaloneImages: Record<string, string> = {
-  // Intro / job context
-  job_setup: "/images/indooroutdoor.png",
+  // Job context
+  "question-5": "/images/indooroutdoor.png",
 
   // Organizational
-  equipment_feedback: "/images/input2.png",
-  work_organization_feedback: "/images/input2.png",
+  "question-7": "/images/input2.png",
+  "question-8": "/images/input2.png",
 
   // Contact stress
-  contact_sharp_edges_duration: "/images/lean.png",
-  kneeling_hard_surfaces: "/images/kneel.jpg",
-  handheld_tool_contact: "/images/handletool30.jpg",
-  [questionIds.bodyMakeshiftTool]: "/images/getty.png",
+  "question-11": "/images/lean.png",
+  "question-12": "/images/kneel.jpg",
+  "question-13": "/images/handletool30.jpg",
+  "question-14": "/images/makeshift.png",
 
   // Force
-  push_pull_surface_types: "/images/trolley.jpg",
-  push_pull_heavy_frequency: "/images/dolly.png",
-  object_weight: "/images/avg_weight.png",
-  tool_start_force: "/images/thumb.png",
-  push_pull_assistance: "/images/assist.png",
+  "question-15": "/images/trolley.jpg",
+  "question-16": "/images/dolly.png",
+  "question-17": "/images/avg_weight.png",
+  "question-18": "/images/thumb.png",
+  "question-19": "/images/assist.png",
 
   // Awkward postures
-  upper_body_posture: "/images/neutralleaning.png",
-  torso_twist: "/images/twisting.png",
-  hand_location: "/images/bodyUpdated.png",
-  arms_extended_forward: "/images/armstretch.jpg",
-  outstretched_arm_load: "/images/armsoutstretched.png",
-  head_position: "/images/headPics.png",
-  wrist_side_angle: "/images/hand.png",
-  objects_close_to_body: "/images/objectClose.png",
+  "question-20": "/images/neutralleaning.png",
+  "question-21": "/images/twisting.png",
+  "question-22": "/images/bodyUpdated.png",
+  "question-23": "/images/armstretch.jpg",
+  "question-24": "/images/armsoutstretched.png",
+  "question-25": "/images/headPics.png",
+  "question-26": "/images/figure.png",
+  "question-27": "/images/hand.png",
+  "question-28": "/images/objectClose.png",
 
   // Repetition
-  repetitive_movements_duration: "/images/similar.jpg",
-  wrist_side_duration: "/images/morethan15.png",
-  forceful_exertion_duration: "/images/muscularforce.jpg",
-  pinch_grip_duration: "/images/pinch2lb.png",
-  power_grip_duration: "/images/powerGrip.png",
-  vibration_tools_duration: "/images/hand_body.png",
-  push_pull_rough_soft_heavy_duration: "/images/surface.png",
+  "question-29": "/images/similar.jpg",
+  "question-30": "/images/figure.png",
+  "question-31": "/images/morethan15.png",
+  "question-32": "/images/muscularforce.jpg",
+  "question-33": "/images/pinch2lb.png",
+  "question-34": "/images/powerGrip.png",
+  "question-35": "/images/hand_body.png",
+  "question-36": "/images/surface.png",
 
   // Environmental
-  noise_distraction: "/images/hearing.jpg",
-  glare: "/images/sunglare.jpg",
-  fine_details: "/images/detail.jpg",
-  cold_environment_discomfort: "/images/cold.jpeg"
+  "question-37": "/images/hearing.jpg",
+  "question-38": "/images/sunglare.jpg",
+  "question-39": "/images/detail.jpg",
+  "question-40": "/images/cold.jpeg"
 };
 
 const groupImages: Record<string, Record<string, string>> = {
